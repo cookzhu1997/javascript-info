29014b76564227576f374d9f395778b59a60b592
cookzhu1997 <cookzhu1997@gmail.com>
2026-07-17 14:11:56 +0800

    JS基础知识9-12

--- 02-JS基础知识/code.js
+++ 02-JS基础知识/code.js
@@ -2,3 +2,24 @@ let admin, name;
 name = "John";
 admin = name;
 console.log(admin)
+
+
+// 好复杂的写法↓
+
+// let age = prompt('age?', 18);
+
+// let message = (age < 3) ? 'Hi, baby!' : (age < 18) ? 'Hello!' : (age < 100) ? 'Greetings!' : 'What an unusual age!';
+
+// console.log( message );
+
+// 好复杂的写法↑
+
+
+let result = "" || "" || "cook" || "wow"
+console.log('逻辑运算符',result)
+
+// 写一个 if 条件句，检查 age 是否不位于 14 到 90 的闭区间。
+
+// 创建两个表达式：第一个用非运算 !，第二个不用。
+if(!(age >= 14 && age <= 90)){}
+if(age<14 || age>90){}
